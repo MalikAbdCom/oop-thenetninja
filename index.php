a35faf9db97cf626372a93c39b9228a2e5b0b7ea
malik abdullah <emailmalikabdcom@gmail.com>
2022-06-16 10:10:51 +0700

    oop inheritance

--- index.php
+++ index.php
@@ -4,14 +4,17 @@ class Maaalik
 {
     private $nama;
     private $alamat;
-    function hiName(){
-        echo $this->nama;
+    protected $salary;
+
+    public function hiName() {
+        return $this->nama;
     }
-    function lastName(){
-        echo $this->alamat;
+
+    public function lastName() {
+        return $this->alamat;
     }
-    function __construct($newName, $newAlamat)
-    {
+
+    public function __construct($newName, $newAlamat) {
         $this->nama = $newName;
         $this->alamat = $newAlamat;
     }
@@ -30,3 +33,46 @@ echo "<br>";
 echo $userTwo->hiName();
 echo "<br>";
 echo $userOne->lastName();
+
+// inherit dari Maaalik
+
+class NewMalik extends Maaalik
+{
+    private $posisi;
+
+    public function __construct($newName, $newAlamat, $posisi, $salary) {
+        parent::__construct($newName, $newAlamat);
+        $this->posisi = $posisi;
+        $this->salary = $salary;
+    }
+
+    public function testPrivate(){
+
+        return $this->name;
+    }
+
+    public function showSalary(){
+        return $this->salary;
+    }
+    public function changeSalary($jumlah){
+        $this->salary = $jumlah;
+    }
+}
+
+echo "<hr>";
+
+$userketiga = new NewMalik("taki", "tokyo", "CEO", "55555");
+
+print_r($userketiga) ;
+echo "<hr>";
+$userketiga->testPrivate();
+echo $userketiga->showSalary();
+echo "<br>";
+
+$userketiga->changeSalary(9999);
+
+echo $userketiga->showSalary();
+
+echo "<br>";
+
+echo $userketiga->hiName() . "punya gaji \$" . $userketiga->showSalary();
