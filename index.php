1a561974ffa71b9315362a1bf57b8df364fff5ec
malik abdullah <emailmalikabdcom@gmail.com>
2022-06-17 20:34:25 +0700

    learn static oop, has error

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 class Maaalik
 {
     protected $nama;
-    private $alamat;
+    private static $alamat = "lubuk linggau";
     protected $salary;
 
-    public function hiName()
+    public static function hiName()
     {
         return $this->nama;
     }
 
-    public function lastName()
+    public static function lastName()
     {
-        return $this->alamat;
+        return self::$alamat;
     }
 
     public function __construct($newName, $newAlamat)
@@ -21,117 +21,8 @@ class Maaalik
         $this->nama = $newName;
         $this->alamat = $newAlamat;
     }
+
+    public static $nama_saya = "malik abdullah";
 }
 
-$userOne = new Maaalik("abdullah", "muara beliti");
-$userTwo = new Maaalik("tiffany", "lubuk linggau");
-
-print_r($userOne);
-echo "<br>";
-print_r($userTwo);
-
-echo "<br>";
-echo $userOne->hiName();
-echo "<br>";
-echo $userTwo->hiName();
-echo "<br>";
-echo $userOne->lastName();
-
-// inherit dari Maaalik
-
-class NewMalik extends Maaalik
-{
-    public $posisi;
-
-    public function __construct($newName, $newAlamat, $posisi, $salary)
-    {
-        parent::__construct($newName, $newAlamat);
-        $this->posisi = $posisi;
-        $this->salary = $salary;
-    }
-
-    public function testPrivate()
-    {
-
-        return $this->nama;
-    }
-
-    public function showSalary()
-    {
-        return $this->salary;
-    }
-
-    public function changeSalary($jumlah)
-    {
-        $this->salary = $jumlah;
-    }
-
-    public function __destruct()
-    {
-       echo "ini pertama, pusingnngg";
-    }
-}
-
-echo "<hr>";
-
-$userketiga = new NewMalik("taki", "tokyo", "CEO", "55555");
-
-echo "user ketiga";
-
-print_r($userketiga);
-
-echo "<hr>";
-echo $userketiga->testPrivate();
-echo $userketiga->showSalary();
-echo "<br>";
-
-$userketiga->changeSalary(9999);
-
-echo $userketiga->showSalary();
-
-echo "<br>";
-
-echo $userketiga->hiName() . "punya gaji \$" . $userketiga->showSalary() . " apa ini coba " . $userketiga->posisi;
-
-
-//unset($userOne);
-class something
-{
-    private $namaku;
-    private $alamatku;
-
-    public function __construct($namaku, $alamatku)
-    {
-        $this->namaku = $namaku;
-        $this->alamatku = $alamatku;
-    }
-
-    public function lolo()
-    {
-        echo $this->namaku;
-    }
-    public function __destruct()
-    {
-        echo "saat terakhir di jalankan";
-    }
-}
-echo "<hr>";
-$userKeempat = new something("bangku", "sumatera");
-echo "<br>";
-print_r($userKeempat);
-echo "<br>";
-echo $userKeempat->lolo();
-echo "<br>";
-
-//unset($userKeempat);
-
-//var_dump($userKeempat);
-
-class pusing {
-    public $aaaa = "asdfasdfas";
-    public function __construct()
-    {
-
-    }
-
-}
+echo Maaalik::lastName();
